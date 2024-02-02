f49a07e13ea85be21ffca3eb712c43afcff66b09
Ankit Sharma <asharma@dainsta.com>
2024-02-02 12:07:36 +0530

    first commit

---
+++ app/snippets/[id]/not-found.tsx
@@ -0,0 +1,7 @@
+export default function SnippetNotFund(){
+    return <div> 
+        <h1 className="text-xl bold"> 
+            Sorry, we could not find the requested snippet.
+        </h1>
+    </div>
+}
---
+++ app/snippets/[id]/page.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { db } from '@/db';
+import { notFound } from 'next/navigation';
+import Link from 'next/link';
+import * as actions from '@/actions';
+
+interface SnippetShowPageProps {
+    params: {
+        id: string,
+    }
+}
+
+const SnippetShowPage = async (props: SnippetShowPageProps) => {
+    const snippet = await db.snippet.findFirst({
+        where: { id: parseInt(props.params.id) }
+    })
+
+    if (!snippet) {
+        return notFound();
+    }
+
+    const deleteSnippetAction = actions.deleteSnippet.bind(null, snippet.id);
+
+    return (
+        <div>
+            <div className='flex m-4 justify-between items-center'>
+                <h1 className='text-xl font-bold'>
+                    {snippet.title}
+                </h1>
+                <div className='flex gap-4'>
+                    <Link href={`${snippet.id}/edit`} className='p-2 border rounded'>Edit </Link>
+                    <form action={deleteSnippetAction}>
+                        <button type='submit' className='p-2 border rounded' >Delete</button>
+                    </form>
+
+                </div>
+            </div>
+            <pre className='p-3 border rounded bg-gray-200 border-gray-200'>
+                <code>{snippet.code}</code>
+            </pre>
+        </div>
+    )
+}
+
+export default SnippetShowPage
